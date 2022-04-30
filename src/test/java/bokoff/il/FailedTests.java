2598c393ea859dfa7da2cbe18226b196cd10d9c0
Ilya Bokov <ibokov@rvision.ru>
2022-04-30 19:01:04 +0300

    added simple test

---
+++ src/test/java/bokoff/il/FailedTests.java
@@ -0,0 +1,33 @@
+package bokoff.il;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class FailedTests {
+
+  @Test
+  void test00(){
+    assertTrue(false);
+  }
+
+  @Test
+  void test01(){
+    assertTrue(false);
+  }
+
+  @Test
+  void test02(){
+    assertTrue(false);
+  }
+
+  @Test
+  void test03(){
+    assertTrue(false);
+  }
+
+  @Test
+  void test04(){
+    assertTrue(false);
+  }
+}
---
+++ src/test/java/bokoff/il/SkippedTests.java
@@ -0,0 +1,30 @@
+package bokoff.il;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SkippedTests {
+
+  @Test
+  @Disabled
+  void test00(){
+    assertTrue(false);
+  }
+
+  @Test
+  @Disabled
+  void test01(){
+    assertTrue(false);
+  }
+
+  @Test
+  @Disabled("some reason")
+  void test02(){
+    assertTrue(false);
+  }
+
+
+}
