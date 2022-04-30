029361b2e6a1199636e55194ecea75290e6a6fba
Ilya Bokov <ibokov@rvision.ru>
2022-04-30 20:05:48 +0300

    added allure attach

--- src/test/java/bokoff/il/demoqa/RegistrationFormTest.java
+++ src/test/java/bokoff/il/demoqa/RegistrationFormTest.java
@@ -4,32 +4,28 @@ import com.codeborne.selenide.Configuration;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.executeJavaScript;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.$;
 import static io.qameta.allure.Allure.step;
 
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 @Tag("demoqa")
-public class RegistrationFormTest {
-
-  @BeforeAll
-  static void setUp(){
-    Configuration.baseUrl = "https://demoqa.com";
-    Configuration.browserSize = "1920x1080";
-
-  }
+public class RegistrationFormTest extends TestBase{
 
   @Test
+  @DisplayName("Successful fill registration test")
   void fillFormTest(){
     String firstName = "Ilya";
     String lastName = "Bokov";
     String email = "bokoff.il@gmail.com";
     String mobilePhone = "1111111111";
-    String subjects="English";
+    String subjects = "English";
     String gender="Male";
     String filePath= "test.jpg";
     String currentAddress="1st Street";
@@ -42,6 +38,8 @@ public class RegistrationFormTest {
 
     step("Open registration form",()->{
       open("/automation-practice-form");
+      executeJavaScript("$('footer').remove()");
+      executeJavaScript("$('#fixedban').remove()");
     });
 
     step("Fill registation form",()->{
