b9bffc995e5e7629d7b23e58a678f97edf463c0d
Ilya Bokov <ibokov@rvision.ru>
2022-05-07 20:47:17 +0300

    owner example

---
+++ src/test/java/bokoff/il/properties/OwnerTest.java
@@ -0,0 +1,20 @@
+package bokoff.il.properties;
+
+import bokoff.il.CredentialsConfig;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class OwnerTest {
+
+  CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
+
+@Test
+@Tag("owner")
+  void loginTest(){
+  String login = config.login();
+  String password = config.password();
+  System.out.println(login);
+  System.out.println(password);
+}
+}
